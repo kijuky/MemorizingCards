7e6b39da563ca8a1f0bac9885da52005fb976583
kijuky <nahaku_789@yahoo.co.jp>
2016-06-14 07:52:03 +0900

    [work] 問題データベースを読み込む処理を追加

--- app/src/main/java/kijuky/fm/memorizingcards/CardActivity.java
+++ app/src/main/java/kijuky/fm/memorizingcards/CardActivity.java
@@ -1,9 +1,13 @@
 package kijuky.fm.memorizingcards;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.RadioButton;
 import android.widget.TextView;
+
+import java.io.IOException;
 
 public class CardActivity extends AppCompatActivity {
 
@@ -12,7 +16,6 @@ public class CardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card);
 
-        // ビュー
         TextView questionSettings = (TextView)findViewById(R.id.textView_question_setting);
         TextView question = (TextView)findViewById(R.id.textView_question);
         assert questionSettings != null;
@@ -20,6 +23,31 @@ public class CardActivity extends AppCompatActivity {
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
+            DataBaseHelper databaseHelper = new DataBaseHelper(this);
+            SQLiteDatabase db = databaseHelper.getWritableDatabase();
+
+            try {
+                Cursor c = db.query("question_t", null, "id=1", null, null, null, null);
+                c.moveToFirst();
+
+                //int id = c.getInt(0);
+                //int year = c.getInt(1);
+                //int month = c.getInt(2);
+                //int number = c.getInt(3);
+                String qs = c.getString(4);
+                String q = c.getString(5);
+                String a1 = c.getString(6);
+                String a2 = c.getString(7);
+                String a3 = c.getString(8);
+                String a4 = c.getString(9);
+                String a5 = c.getString(10);
+                int a = c.getInt(11);
+
+                c.close();
+            } finally {
+                db.close();
+            }
+
             questionSettings.setText("問題設定");
             question.setText("問題");
         }
---
+++ app/src/main/java/kijuky/fm/memorizingcards/DataBaseHelper.java
@@ -0,0 +1,73 @@
+package kijuky.fm.memorizingcards;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.channels.Channel;
+
+/**
+ * Created by admin on 2016/06/13.
+ */
+public class DataBaseHelper extends SQLiteOpenHelper {
+
+    private static final String DB_FILE_NAME = "question.db";
+    private static final String DB_NAME = "question";
+    private static final int DB_VERSION = 1;
+
+    private Context context;
+    private File path;
+    private boolean initialized;
+
+    public DataBaseHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+        this.context = context;
+        this.path = context.getDatabasePath(DB_NAME);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        super.onOpen(db);
+    }
+
+    @Override
+    public synchronized SQLiteDatabase getWritableDatabase() {
+        SQLiteDatabase db = super.getWritableDatabase();
+        if (!initialized) {
+            try {
+                db.close();
+                db = copyDatabaseFromAssets();
+                initialized = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return db;
+    }
+
+    private SQLiteDatabase copyDatabaseFromAssets() throws IOException {
+        InputStream input = context.getAssets().open(DB_FILE_NAME);
+        OutputStream output = new FileOutputStream(path);
+
+        byte[] buffer = new byte[1024];
+        int size;
+        while ((size = input.read(buffer)) > 0) {
+            output.write(buffer, 0, size);
+        }
+        output.flush();
+        output.close();
+        input.close();
+
+        return super.getWritableDatabase();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // do nothing.
+    }
+}
